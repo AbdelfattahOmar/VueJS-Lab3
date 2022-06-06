150e248ee3079e1b24f1a9dba7f9402dcc5a417c
AbdlefattahOmar <abdlefattahomar66@gmail.com>
2022-06-06 22:43:06 +0200

    First Task

---
+++ src/components/ContactComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Contact</h1>
+    <div>
+      <span class="h5">Phone Number:</span>
+      <p>01024204092</p>
+    </div>
+    <div>
+      <span class="h5">Email:</span>
+      <p>abdelfattahomar66@gmail.com</p>
+    </div>
+  </div>
+</template>
---
+++ src/components/GalleryComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="row">
+    <h1 class="col-12">Gallery</h1>
+    <div class="col-12 row">
+      <img
+        src="https://images.unsplash.com/photo-1500622944204-b135684e99fd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bmF0dXJhbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+        class="col-5 me-4"
+        alt=""
+      />
+      <img
+        src="https://images.unsplash.com/photo-1500828131278-8de6878641b8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fG5hdHVyYWx8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+        class="col-5"
+        alt=""
+      />
+    </div>
+  </div>
+</template>
